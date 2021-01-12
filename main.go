9852c04fca589c5f56673ea9610be9aa1f9068c3
Oleh Kovalyshyn <kovalyshinO@gmail.com>
2021-01-12 12:38:42 +0200

    Changed main.go

--- main.go
+++ main.go
@@ -2,26 +2,17 @@ package main
 
 import (
 	"flag"
-	"log"
-	"tree/ntrees"
+	"tree/cmd/client"
+	"tree/cmd/server"
 )
 
 var clientMode = flag.Bool("c", false, "Enable client mode")
 
 func main() {
-	//flag.Parse()
-	//if *clientMode {
-	//	client.RunClient()
-	//} else {
-	//	server.RunServer()
-	//}
-
-	ntree, err := ntrees.FromFile("ntree.json")
-	if err != nil {
-		log.Fatal(err)
-	}
-	ntree.Op()
-	if err := ntrees.ToFile(ntree, "ntree2.json"); err != nil {
-		log.Fatal(err)
+	flag.Parse()
+	if *clientMode {
+		client.RunClient()
+	} else {
+		server.RunServer()
 	}
 }
